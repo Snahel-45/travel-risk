9a2cc5fe5e31f6f4b2f4f11542bb82d18461535a
Snahel-45 <112778038+Snahel-45@users.noreply.github.com>
2025-08-19 15:16:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sliders = document.querySelectorAll(".slider-container");
+
+  sliders.forEach((sliderContainer) => {
+    const slider = sliderContainer.querySelector(".slider");
+    const leftBtn = sliderContainer.querySelector(".arrow.left");
+    const rightBtn = sliderContainer.querySelector(".arrow.right");
+
+    rightBtn.addEventListener("click", () => {
+      const cardWidth = slider.querySelector(".card").offsetWidth + 20; // recalc
+      slider.scrollBy({ left: cardWidth, behavior: "smooth" });
+    });
+
+    leftBtn.addEventListener("click", () => {
+      const cardWidth = slider.querySelector(".card").offsetWidth + 20; // recalc
+      slider.scrollBy({ left: -cardWidth, behavior: "smooth" });
+    });
+  });
+});
